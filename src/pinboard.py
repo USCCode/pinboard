4e71229d067bb65008f4ec1aaafdd31a9ba87fc4
Jose M Vidal <jmvidal@gmail.com>
2012-05-17 14:25:27 -0400

    HW2: forms and templates
    We ask the user for the image URL and a caption then show him the one he
    just entered.
    
    Use jinja2 templates. Use form with GET method. User self.request.

--- src/pinboard.py
+++ src/pinboard.py
@@ -1,19 +1,25 @@
 '''
+Pinboard : a CSCE 242 project.
 Created on May 16, 2012
+www.csce242.com
 
-@author: jmvidal
+@author: Jose M Vidal <jmvidal@gmail.com>
 '''
 import webapp2
 import jinja2
 import os
+import logging #for debugging.
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         templateValues = {}
-        if self.request.get('img-url') != None:
-            templateValues['img-url'] = self.request.get('img-url') 
+        if self.request.get('imgUrl') != None:
+            templateValues['imgUrl'] = self.request.get('imgUrl')
+        if self.request.get('caption') != None:
+            templateValues['caption'] = self.request.get('caption')
+        templateValues['title'] = 'Homework 1'            
         template = jinja_environment.get_template('main.html')
         self.response.out.write(template.render(templateValues))
 
