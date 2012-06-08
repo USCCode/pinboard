58ea0331e3ba923af52bc9ba5b05e87cd8080c31
Jose M Vidal <jmvidal@gmail.com>
2012-06-08 17:50:36 -0400

    Eliminate duplicate images.

--- src/js/bookmarklet.js
+++ src/js/bookmarklet.js
@@ -3,9 +3,9 @@ var pinboardHostname = 'localhost:8080';
 function generateNewPage(){
 	//Get all the images from the current page
 	var imgs = document.getElementsByTagName('img');
-	var imgUrls = []; //copy the urls into this array
+	var imgUrls = {}; //make the urls into this object's properties, thus eliminating duplicates 
 	for (var i = 0; i < imgs.length; i ++){
-		imgUrls.push(imgs[i].src);
+		imgUrls[imgs[i].src] = 1; 
 	};
 	var html = document.getElementsByTagName('html')[0];
 	var body = document.getElementsByTagName('body')[0];
@@ -14,12 +14,12 @@ function generateNewPage(){
 	var newHtml = '<html><head></head><body><h1>Pinboard</h2>' +
 				 '<p>Pick from one of these images:</p><p>';
 	console.log(imgs.length);
-	for (var i = 0; i < imgUrls.length; i++){
+	for (var url in imgUrls){
 		console.log(i);
 		newHtml = newHtml + '<div><img style="width:200px" src="' 
-			    + imgUrls[i] + '"/><br/><form method="post" action="http://' 
+			    + url + '"/><br/><form method="post" action="http://' 
 				+ pinboardHostname +'/pin/">' 
-				+ '<input type="hidden" name="imgUrl" value="' + imgUrls[i]
+				+ '<input type="hidden" name="imgUrl" value="' + url
 		        +  '"/><input type="submit" value="This One"/></form></div>&nbsp;';
 	};
 	newHtml = newHtml + '</p>';
