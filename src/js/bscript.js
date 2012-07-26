4ec3c28e7820354ff171dc4eefcce85c6082d940
Jose M Vidal <jmvidal@gmail.com>
2012-07-26 11:23:05 -0400

    Add button moves image to board.

--- src/js/bscript.js
+++ src/js/bscript.js
@@ -5,16 +5,73 @@ function viewPin(pin){
         'Pin '+ pin.pinid + '<br/>' +
         '<span class="displayCaption" id="caption">' +
         pin.caption + 
-        '</span><p style="clear:both"></p></div>';
+        '</span></div>';
 }
 
 
-function drawBoard(data){
-	$('#boardTitle').text(data.title);
-	for (var i=0; i < data.pins.length; i++){
-		var pin = data.pins[i];
+function viewPinToAdd(pin){
+		return'<div id="addpin'+ pin.pinid + '" class="addbox"><a href="/pin/'+ pin.pinid +'">' +
+		'<img class="thumbPin" title="Pin '+ pin.pinid +'" alt="'+ pin.caption +'" src="'+
+		pin.imgUrl + '"/></a>' +
+        'Pin '+ pin.pinid + '<br/>' +
+        '<span class="displayCaption" id="caption">' +
+        pin.caption + 
+        '</span><br/><input onclick="addPin('+ pin.pinid+')" type="button" value="Add"></input></div>';
+}
+
+function addPin(pinNumber){
+	movePinToBoard(pinNumber);
+	drawBoard();
+	drawExtrapins();
+}
+
+function movePinToBoard(pinNumber){
+	console.log('add' + pinNumber);
+	var pin;
+	for (var i=0; i < allPins.length; i++){
+		if (allPins[i].pinid == pinNumber){
+			pin = allPins[i];
+			board.pins.push(pin);
+		}
+	}
+}
+
+function drawBoard(){
+	$('#boardTitle').text(board.title);
+	$('#pins').html('');
+	for (var i=0; i < board.pins.length; i++){
+		var pin = board.pins[i];
 		var pinHtml = viewPin(pin);
 		$('#pins').append(pinHtml);
+	}
+}
+
+/**
+ * Determine if pinid is in board, 
+ * @param pinid
+ * @returns the pin, or undefined if not in.
+ */
+function getBoardPin(pinid){
+	for (var i=0; i < board.pins.length; i++){
+		var pin = board.pins[i];
+		if (pin.pinid == pinid) { return pin};
+	}
+	return undefined;
+}
+
+/**
+ * Draw 'allPins' in the pins-to-add section
+ * @returns
+ */
+function drawExtrapins(){
+	console.log('viewextra');
+	$('#pinstoadd').html('');
+	for (var i=0; i < allPins.length; i++){
+		if (getBoardPin(allPins[i].pinid) == undefined ){
+			console.log('pin=' + i);
+			var pinhtml = viewPinToAdd(allPins[i]);
+			$('#pinstoadd').append(pinhtml);
+		};
 	}
 }
 
@@ -34,6 +91,7 @@ function getBoard(){
 		type: 'GET',
 		success: function(data){
 			allPins = data;
+			drawExtrapins();
 		}
 	});
 }
