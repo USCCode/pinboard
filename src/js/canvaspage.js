89641125f5126cdc37c327a9f1f78945558cacc7
Jose M Vidal <jmvidal@gmail.com>
2012-09-15 08:46:03 -0400

    Drag around one Image in the canvas.
    It is very flickery. I don't see that there is any simple way to reduce
    the flicker as long as we clear the whole canvas every time. One
    solution might be to clear only the img rectangle that is being moved
    but that will get complicated because the img could be overlapping other
    ones, so we have to re-draw those (and only those). Do-able, but more
    complicated than I want to deal with now.

--- src/js/canvaspage.js
+++ src/js/canvaspage.js
@@ -1,34 +1,64 @@
 /**
- * Returns a function that when called will draw 'this' Image on ctx at x=i*100.
- * Explanation: This function captures ctx and i in a closure. So if i changes later
+ * Returns a function that when called will draw 'this' Image on context at x=i*100.
+ * Explanation: This function captures i in a closure. So if i changes later
  *  it is not a problem, because this i is now local.
- * @param ctx
  * @param i
  * @returns {Function}
  */
-function drawAt(ctx,i){
+function drawAt(x,y){
 	return function(){
-		ctx.drawImage(this, i*100, 0, 100, 100);
+		context.drawImage(this, x, y, 100, 100);
 	}
 };
+
+/**
+ * Board model, in JSON format.
+ */
+var boardModel = {};
+
+function drawPin(theBoard,pinNumber){
+	var pin = theBoard.pins[pinNumber];
+	var img = new Image();
+	img.onload = drawAt(Math.floor(pin.x), Math.floor(pin.y)); 
+	img.src = pin.imgUrl;
+}
 
 /**
  * Add all the board's pin to the canvas object;
  * @param theBoard
  */
 function drawBoard(theBoard){
+	console.log("drawBoard");
 	$('#boardTitle').text(theBoard.title);
-	var canvas = document.getElementById('board');
-	var ctx = canvas.getContext('2d');
+	context.clearRect(0,0,canvas.width,canvas.height);
 	for (var i=0; i < theBoard.pins.length; i++){
-		var pin = theBoard.pins[i];
-		var img = new Image();
-		img.onload = drawAt(ctx,i); 
-		img.src = pin.imgUrl;
-		//add pin to canvas
+		drawPin(theBoard,i);
+//		var pin = theBoard.pins[i];
+//		var img = new Image();
+//		img.onload = drawAt(pin.x, pin.y); 
+//		img.src = pin.imgUrl;
 	}
 }
 
+function getPosInCanvas(evt) {
+	var rect = canvas.getBoundingClientRect();
+    var doc = document.documentElement;
+    // return relative mouse position
+    var mouseX = evt.clientX - rect.left - doc.scrollLeft;
+    var mouseY = evt.clientY - rect.top - doc.scrollTop;
+    return {
+      x: mouseX,
+      y: mouseY
+    };
+}
+
+
+/***
+ * Global var to hold the 2D context.
+ */
+var context;
+var canvas;
+var chosenPin = 0;
 
 /**
  * Get all the board data, and all the user's pins, from the server.
@@ -39,16 +69,39 @@ function getBoard(){
 	$.ajax('/board/' + boardid + '.json', {
 		type: 'GET',
 		success: function(data){
-			drawBoard(data);
+			boardModel = data;
+			for (var i=0; i < boardModel.pins.length; i++){
+				boardModel.pins[i].x = Math.random()*(canvas.width-100);
+				boardModel.pins[i].y = Math.random()*(canvas.height-100);
+			}
+			drawBoard(boardModel);
 		},
 		error: function(e){
 			console.log("Could not get board data from server...");
 		}
-		
 	});
+}
+
+
+
+function mouseMoveHandler(evt){
+	var xy = getPosInCanvas(evt);
+	console.log("Mouse moved" + xy.y);
+	context.clearRect(0,0,canvas.width,canvas.height);
+	boardModel.pins[chosenPin].x = xy.x;
+	boardModel.pins[chosenPin].y = xy.y;
+	drawBoard(boardModel);	
+}
+
+function mouseClickHandler(evt){
+	console.log("Click");
 }
 
 $(document).ready(function(){
 	console.log("Ready to rock...");
+	$('#board').on('mousemove',mouseMoveHandler);
+	$('#board').on('click',mouseClickHandler);
+	canvas = document.getElementById('board');
+	context = canvas.getContext('2d');	
 	getBoard();
 });
